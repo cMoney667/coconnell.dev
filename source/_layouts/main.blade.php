c03f04544ffed0d3e4b0a7c995c3d124d0696859
shortnd <ococncol@gmail.com>
2023-09-09 23:07:17 -0400

    init

---
+++ source/_layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ $page->language ?? 'en' }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="canonical" href="{{ $page->getUrl() }}">
+        <meta name="description" content="{{ $page->description }}">
+        <title>{{ $page->title }}</title>
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+    </head>
+    <body>
+        <header>
+            <h1>Collin O'Connell</h1>
+        </header>
+        <main>
+            @yield('body')
+        </main>
+        <footer>
+            Copyright &copy;
+        </footer>
+    </body>
+</html>
