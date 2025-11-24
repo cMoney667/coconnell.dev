671450351fd56733cbe5017c60198efc22e6f31b
Collin <ococncol@gmail.com>
2025-11-24 13:51:53 -0500

    moving to hyde

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
+// Using Vite is optional, as the styles you need to get started are already included.
+// However, if you customize existing or add new Tailwind classes, you can use Vite
+// to compile the assets. See https://hydephp.com/docs/1.x/managing-assets.html.
+
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
+import hyde from 'hyde-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+        hyde({
+            input: ['resources/assets/app.css', 'resources/assets/app.js'],
+            watch: ['_pages', '_posts', '_docs'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
