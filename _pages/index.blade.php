c8824eb8abb9cf007095259e25b6cbff7e91087e
cMoney667 <ococncol@gmail.com>
2025-11-24 15:16:02 -0500

    updating size of heading

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -3,8 +3,8 @@
 @php($title = "Collin OConnell")
 
 <main class="mx-auto max-w-7xl py-16 px-8 text-center">
-    <h1 class="text-6xl font-bold">Collin OConnell</h1>
-    <h2 class="text-5xl">Lead Developer</h2>
+    <h1 class="text-5xl font-bold">Collin OConnell</h1>
+    <h2 class="text-4xl">Lead Developer</h2>
 </main>
 
 @endsection
